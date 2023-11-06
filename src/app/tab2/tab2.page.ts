f5c10efb92668979dd40d18f8e66167c21435b18
shivnandan-Gunagya <shivnandansoni@gunagya.in>
2023-11-06 14:00:48 +0530

    Ble working

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import { BleClient, ScanResult } from '@capacitor-community/bluetooth-le';
 import {
+  BleClient,
+  dataViewToText,
+  ScanResult,
+  textToDataView,
+} from '@capacitor-community/bluetooth-le';
+import {
+  IonButton,
+  IonInput,
   LoadingController,
   ModalController,
   ToastController,
@@ -13,11 +20,22 @@ import {
   styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  @ViewChild('inputField')
+  inputField!: IonInput;
+  @ViewChild('sendButton')
+  sendButton!: IonButton;
+  inputVisible = false;
+  newText: any = null;
+  dataView: any = null;
+
+  userInput: string = '';
   loaderToShow: any;
   bluetoothScanResults: ScanResult[] = [];
   bluetoothIsScanning = false;
-
   bluetoothConnectedDevice?: ScanResult;
+  connectedDevice: any = '';
+  ESP_SERVICE = '6e400001-b5a3-f393-e0a9-e50e24dcca9e'.toUpperCase();
+  ESP_CHARACTERISTIC_SERVICE = '6e400003-b5a3-f393-e0a9-e50e24dcca9e'.toUpperCase();
 
   constructor(
     public toastController: ToastController,
@@ -26,7 +44,21 @@ export class Tab2Page {
     private loadingCtrl: LoadingController
   ) {}
 
-  ngOnInit(): void {}
+  toggleInputField() {
+    this.inputVisible = !this.inputVisible;
+
+    if (this.inputVisible) {
+      this.inputField.setFocus();
+    }
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 1700,
+    });
+    toast.present();
+  }
 
   showLoader(text: any) {
     this.loaderToShow = this.loadingCtrl
@@ -45,6 +77,9 @@ export class Tab2Page {
     this.loadingCtrl.dismiss();
   }
 
+  ngOnInit(): void {}
+
+  // Scan Function
   async scanForBluetoothDevices() {
     try {
       await BleClient.initialize({ androidNeverForLocation: true });
@@ -68,8 +103,6 @@ export class Tab2Page {
       await BleClient.requestLEScan({}, (result: any) => {
         console.log('Received new scan result', result);
 
-        console.log('MData', result);
-
         this.bluetoothScanResults.push(result);
         this.hideLoader();
       });
@@ -86,47 +119,76 @@ export class Tab2Page {
     }
   }
 
-  enableBt() {
-    BleClient.enable().then(
-      () => {
-        this.scanForBluetoothDevices();
-      },
-      () => {
-        alert('Allow Bluetooth To Turn On Or Exit The App!');
+  // Enable Bluetooth function
+
+  async enableBt() {
+    try {
+      await BleClient.enable().then(
+        () => {
+          this.scanForBluetoothDevices();
+        },
+        () => {
+          alert('Allow Bluetooth To Turn On Or Exit The App!');
+        }
+      );
+    } catch (error) {
+      alert('please turn on Bluetooth manulally');
+    }
+  }
+
+  // Connect Function
+
+  async connectToBluetoothDevice(scanResult: any) {
+    this.showLoader(`connecting with ${scanResult.name}`);
+    try {
+      await BleClient.connect(scanResult.deviceId);
+
+      this.connectedDevice = scanResult;
+      this.readData();
+      this.bluetoothConnectedDevice = scanResult.name;
+      this.hideLoader();
+      if (this.bluetoothConnectedDevice === scanResult.name) {
+        alert('connected');
       }
-    );
+    } catch (error) {
+      this.hideLoader();
+      alert('can not connect with the device');
+      console.error('connectToDevice', error);
+      // this.presentToast(JSON.stringify(error));
+    }
   }
 
-  onBluetooDeviceDisconnected(disconnectedDeviceId: string) {
-    alert(`Diconnected ${disconnectedDeviceId}`);
-    this.bluetoothConnectedDevice = undefined;
-  }
+  //Write a message
 
-  async presentToast(message: string) {
-    const toast = await this.toastController.create({
-      message,
-      duration: 1700,
-    });
-    toast.present();
-  }
-
-  async connectToBluetoothDevice(scanResult: ScanResult) {
-    const device = scanResult.device;
-
-    alert(scanResult);
+  async writeData() {
+    this.newText = textToDataView(this.userInput);
+    this.dataView = dataViewToText(this.newText);
 
     try {
-      await BleClient.connect(
-        device.deviceId,
-        this.onBluetooDeviceDisconnected.bind(this)
+      await BleClient.write(
+        this.connectedDevice.deviceId,
+        this.ESP_SERVICE,
+        this.ESP_CHARACTERISTIC_SERVICE,
+        this.newText
       );
-
-      this.bluetoothConnectedDevice = scanResult;
-      const deviceName = device.name ?? device.deviceId;
-      this.presentToast(`Connected to device ${deviceName}`);
+      alert('message sent sucesfully!');
     } catch (error) {
-      console.error('connectToDevice', error);
-      this.presentToast(JSON.stringify(error));
+      alert('can not send the message');
+    }
+  }
+
+  //Read Characteristic
+
+  async readData() {
+    try {
+      const esp_characterictic = await BleClient.read(
+        this.connectedDevice.deviceId,
+        this.ESP_SERVICE,
+        this.ESP_CHARACTERISTIC_SERVICE
+      );
+      alert(esp_characterictic.getUint8(0));
+    } catch (error) {
+      alert('got an error while reading the data');
     }
   }
 }
